774b26abfe6e289e5f2edf6ea9c4b4cd0bd9b9ce
laurentsata <sataward666@gmail.com>
2022-06-15 16:19:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React,{useState} from 'react';
+import DisplaySimpson from './components/DisplaySimpson';
 import './App.css';
 
+const sampleSimpson = {
+  character: '',
+  
+  quote: '',
+  image: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
+
 function App() {
+  const getSimpson = () => {
+    // Send the request
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        setSimpson(data[0]);
+      });
+  };
+
+    const [simpson, setSimpson] = useState(sampleSimpson);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplaySimpson simpson={simpson} />
+      <button type="button" onClick={getSimpson}>Get simpson</button>
     </div>
   );
 }
---
+++ src/components/DisplaySimpson.jsx
@@ -0,0 +1,23 @@
+// src/components/DisplaySimpson.jsx
+import React from 'react';
+
+function DisplaySimpson({ simpson }) {
+  return (
+    simpson && (
+      <div className='DisplaySimpson'>
+        <img src={simpson.image} alt={simpson.character} />
+        <ul>
+          
+          <li>
+            Name: {simpson.character}
+            
+          </li>
+          <li>Quote: {simpson.quote}</li>
+          
+        </ul>
+      </div>
+    )
+  );
+}
+
+export default DisplaySimpson;
